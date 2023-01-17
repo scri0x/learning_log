bd0c08937983a2e8f15949e961d0562e658a499c
scri0x <1warmn@bk.ru>
2023-01-18 01:40:11 +0500

    user app

---
+++ learning_log/user/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import *
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register, name='register')
+]
---
+++ learning_log/user/views.py
@@ -0,0 +1,29 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth import logout, authenticate, login
+from django.contrib.auth.forms import UserCreationForm
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+def register(request):
+    if request.method != 'POST':
+        form = UserCreationForm()
+    else:
+        form = UserCreationForm(data=request.POST)
+
+    if form.is_valid():
+        new_user = form.save()
+        authenticated_user = authenticate(username=new_user.username, password=request.POST['password1'])
+        login(request, authenticated_user)
+        return HttpResponseRedirect(reverse('index'))
+    context = {
+        'title': 'Регистрация',
+        'form': form,}
+    return render(request, 'register.html', context)
+
+    
